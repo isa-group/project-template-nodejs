ee77a6f8440687697c8387f9733bbfffe3dd0f5f
Antonio Gamez Diaz <agamez2@us.es>
2018-03-27 09:01:00 +0200

    Update index.js with some express stuff

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var logger = require('./src/backend/logger');
 
 //if your project is a npm module:
 
-module.exports = src;
+module.exports = server;
 
 //if your is not npm module
 
-src.myPromiseFunction("1", "2").then(function (solution) {
+server.myPromiseFunction("1", "2").then(function (solution) {
     logger.info(solution);
 }, function (err) {
     logger.error(err);
@@ -17,23 +17,23 @@ src.myPromiseFunction("1", "2").then(function (solution) {
 
 // quit on ctrl-c when running docker in terminal
 process.on('SIGINT', function onSigint() {
-    console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+    logger.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
     shutdown();
 });
 
 // quit properly on docker stop
 process.on('SIGTERM', function onSigterm() {
-    console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+    logger.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
     shutdown();
-})
+});
 
 // shut down server
 function shutdown() {
     server.close(function onServerClosed(err) {
         if (err) {
-            console.error(err);
+            logger.error(err);
             process.exitCode = 1;
         }
         process.exit();
-    })
+    });
 }
