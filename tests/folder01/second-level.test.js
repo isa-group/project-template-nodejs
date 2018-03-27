4f24712f634dcdb70771ea9b9480befa861e9cd3
Antonio Gamez Diaz <agamez2@us.es>
2018-03-27 08:53:30 +0200

    Renamed demo test and removed logs.json, added .nocontent instead

--- tests/folder01/second-level.test.js
+++ tests/folder01/second-level.test.js
@@ -1,6 +1,6 @@
 /*!
-project-template-nodejs 0.0.0, built on: 2017-03-30
-Copyright (C) 2017 ISA group
+project-template-nodejs 0.0.0, built on: 2018-03-26
+Copyright (C) 2018 ISA group
 http://www.isa.us.es/
 https://github.com/isa-group/project-template-nodejs
 
